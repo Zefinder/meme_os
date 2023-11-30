04b4e3cde33d7b1c02997be53c58cc98204e8fdf
nicolasmggRigal <nicolasmgg.rigal@gmail.com>
2023-11-30 17:15:19 +0100

    corrected paging, beautified paging, TODO manage idt and all

--- kernel/include/extend/pagemem.h
+++ kernel/include/extend/pagemem.h
@@ -12,7 +12,19 @@
 #define cr0_pg_en(_cr0_)				\
 	{(_cr0_)->raw |= CR0_PG;}
 
-#define pgd_krn_pde(_pde_,_ptb_)				\
+
+/***********************************************/
+/**    Initializes PDE with given PTB         **/
+/**                                           **/
+/**    PDE entries are aligned with 0x1000    **/
+/**                                           **/
+/**    Suffix specifies pages' privilege      **/
+/**    level                                  **/
+/**                                           **/
+/**    _pgd_  : address of PGD                **/
+/**    _ptb_  : address of PTB                **/
+/***********************************************/
+#define set_pde_krn(_pde_,_ptb_)				\
 	{											\
 		(_pde_)->raw 	= (offset_t)_ptb_;		\
 		(_pde_)->p		= 1;					\
@@ -25,7 +37,7 @@
 		(_pde_)->avl	= 0;					\
 	}
 
-#define pgd_usr_pde(_pde_,_ptb_)				\
+#define set_pde_usr(_pde_,_ptb_)				\
 	{											\
 		(_pde_)->raw 	= (offset_t)_ptb_;		\
 		(_pde_)->p		= 1;					\
@@ -38,6 +50,17 @@
 		(_pde_)->avl	= 0;					\
 	}
 
+/***********************************************/
+/**    Initializes PDE with given address     **/
+/**                                           **/
+/**    PTE entries are aligned with 0x0010    **/
+/**                                           **/
+/**    Suffix specifies pages' privilege      **/
+/**    level                                  **/
+/**                                           **/
+/**    _pgd_  : address of PGD                **/
+/**    _ptb_  : address of PTB                **/
+/***********************************************/
 #define ptb_krn_pte(_pte_,_phys_)							\
 	pg_set_entry(_pte_, PG_RW, pg_4K_nr(_phys_))
 
@@ -46,19 +69,19 @@
 
 
 /*************************************************/
-/**    Initializes PDE with identity mapping    **/
+/**    Initializes PDE                          **/
 /**                                             **/
 /**    PDE entries are aligned with 0x1000      **/
 /**                                             **/
-/**    _pgd_ : address of PGD                   **/
-/**    _ptb_ : address of PTB                   **/
+/**    _pgd_  : address of PGD                  **/
+/**    _ptbs_ : address of PGD's PTBs           **/
 /**    _virt_ : virtual address to map          **/
 /*************************************************/
-#define pgd_krn_identity_pde(_pgd_,_ptb_,_virt_)							\
-	pgd_krn_pde( &(_pgd_)[pd32_idx(_virt_)], &(_ptb_)[pt32_idx(_virt_)] )
+#define pgd_set_krn_pde(_pgd_,_ptbs_,_virt_)														\
+	set_pde_krn( &(_pgd_)[pd32_idx(_virt_)], nth_ptb(_ptbs_, pd32_idx(_virt_)) )
 
-#define pgd_usr_identity_pde(_pgd_,_ptb_,_virt_)							\
-	pgd_usr_pde( &(_pgd_)[pd32_idx(_virt_)], &(_ptb_)[pt32_idx(_virt_)] )
+#define pgd_set_usr_pde(_pgd_,_ptbs_,_virt_)														\
+	set_pde_usr( &(_pgd_)[pd32_idx(_virt_)], nth_ptb(_ptbs_, pd32_idx(_virt_)) )
 
 
 /*************************************************/
@@ -66,14 +89,14 @@
 /**                                             **/
 /**    PTE entries are aligned with 0x10        **/
 /**                                             **/
-/**    _ptb_ : address of PTB                   **/
+/**    _ptbs_ : address of PTBs                 **/
 /**    _virt_ : virtual address to map          **/
 /*************************************************/
-#define ptb_krn_identity_pte(_ptb_,_virt_)				\
-	ptb_krn_pte( &(_ptb_)[pt32_idx(_virt_)], _virt_ )
+#define ptb_set_krn_identity_pte(_ptbs_,_virt_)											\
+	ptb_krn_pte( &nth_ptb(_ptbs_, pd32_idx(_virt_))[pt32_idx(_virt_)], _virt_ )
 
-#define ptb_usr_identity_pte(_ptb_,_virt_)				\
-	ptb_usr_pte( &(_ptb_)[pt32_idx(_virt_)], _virt_ )
+#define ptb_set_usr_identity_pte(_ptbs_,_virt_)											\
+	ptb_usr_pte( &nth_ptb(_ptbs_, pd32_idx(_virt_))[pt32_idx(_virt_)], _virt_ )
 
 
 /**************************************************/
@@ -84,81 +107,90 @@
 /**                                              **/
 /**     PTE entries are aligned with 0x10        **/
 /**                                              **/
-/**    _ptb_ : address of PTB                    **/
+/**    _ptbs_ : address of PTBs                  **/
 /**    _virt_ : virtual address to map           **/
 /**    _phys_ : physical address to map          **/
 /**************************************************/
-#define ptb_krn_forced_pte(_ptb_,_virt_,_phys_)			\
-	ptb_krn_pte( &(_ptb_)[pt32_idx(_virt_)], _phys_ )
+#define ptb_set_krn_forced_pte(_ptbs_,_virt_,_phys_)								\
+	ptb_krn_pte( &nth_ptb(_ptbs_, pd32_idx(_virt_))[pt32_idx(_virt_)], _phys_ )
 
-#define ptb_usr_forced_pte(_ptb_,_virt_,_phys_)			\
-	ptb_usr_pte( &(_ptb_)[pt32_idx(_virt_)], _phys_ )
+#define ptb_set_usr_forced_pte(_ptbs_,_virt_,_phys_)								\
+	ptb_usr_pte( &nth_ptb(_ptbs_, pd32_idx(_virt_))[pt32_idx(_virt_)], _phys_ )
 
 
 /**************************************/
 /**    Returns the address of the    **/
 /**        nth PGD in memory         **/
 /**************************************/
-#define nth_pgd(n)	(pde32_t*)(PGD_START + n*PGD_SIZE)
+#define nth_pgd_gbl(n)	((pde32_t*)(PGD_START + n*PGD_SIZE))
 
 
 /**************************************/
 /**    Returns the address of the    **/
 /**        nth PTB in memory         **/
 /**************************************/
-#define nth_ptb(n)	(pte32_t*)(PTB_START + n*PTB_SIZE)
+#define nth_ptb_gbl(n)	((pte32_t*)(PTB_START + n*PTB_SIZE))
+
+
+/******************************************/
+/**    Returns the address of the        **/
+/**    nth PTB in a flat array of PTBs   **/
+/******************************************/
+#define nth_ptb(_ptbs_,n)	((pte32_t*)((offset_t)(_ptbs_) + n*PTB_SIZE))
 
 
 /******************************************************/
 /**    Maps memory with identity mapping             **/
 /**                                                  **/
-/**    _pgd_   : address of PGD to use               **/
-/**    _ptb_   : address of PTB to use               **/
+/**    _pgd_   : address of used PGD                 **/
+/**    _ptbs_  : address of PGD's PTBs               **/
 /**    _start_ : start address of desired mapping    **/
 /**    _end_   : last byte of the end address of     **/
 /**              desired mapping                     **/
 /**                                                  **/
-/**    Example : to map Kernel space :               **/
+/**    Prefix specifies pages' privilege level       **/
+/**                                                  **/
+/**    Example: to map Kernel space:                 **/
 /**    krn_identity_map(KERNEL_START, KERNEL_END)    **/
 /******************************************************/
-#define krn_identity_map(_pgd_,_ptb_,_start_,_end_)										\
-	{																		\
-		for(offset_t p = _start_; p <= _end_; p += PGD_OFFSET) {			\
-			pgd_krn_identity_pde((_pgd_), (_ptb_), p);						\
-		}																	\
-		for(offset_t p = _start_; p <= _end_; p += PTB_OFFSET) {			\
-			ptb_krn_identity_pte((_ptb_), p);								\
-		}																	\
+#define krn_identity_map(_pgd_,_ptbs_,_start_,_end_)							\
+	{																			\
+		for(offset_t p = _start_; p <= _end_; p += PDE_OFFSET) {				\
+			pgd_set_krn_pde( ((pde32_t*)(_pgd_)), ((pde32_t*)(_ptbs_)), p );	\
+		}																		\
+		for(offset_t p = _start_; p <= _end_; p += PTE_OFFSET) {				\
+			ptb_set_krn_identity_pte( ((pde32_t*)(_ptbs_)), p );				\
+		}																		\
 	}
 
-#define usr_identity_map(_pgd_,_ptb_,_start_,_end_)							\
-	{																		\
-		for(offset_t p = _start_; p <= _end_; p += PGD_OFFSET) {			\
-			pgd_usr_identity_pde((_pgd_), (_ptb_), p);						\
-		}																	\
-		for(offset_t p = _start_; p <= _end_; p += PTB_OFFSET) {			\
-			ptb_usr_identity_pte((_ptb_), p);								\
-		}																	\
+#define usr_identity_map(_pgd_,_ptbs_,_start_,_end_)							\
+	{																			\
+		for(offset_t p = _start_; p <= _end_; p += PDE_OFFSET) {				\
+			pgd_set_usr_pde( ((pde32_t*)(_pgd_)), ((pde32_t*)(_ptbs_)), p );	\
+		}																		\
+		for(offset_t p = _start_; p <= _end_; p += PTE_OFFSET) {				\
+			ptb_set_usr_identity_pte( ((pde32_t*)(_ptbs_)), p );				\
+		}																		\
 	}
 
 
 /***********************************************************/
 /**    Maps memory with forced mapping                    **/
 /**                                                       **/
-/**    _pgd_   : address of PGD to use                    **/
-/**    _ptb_   : address of PTB to use                    **/
-/**    _start_ : logical start address of mapping         **/
-/**    _start_ : physical start address of mapping        **/
-/**    _size_  : size in bytes of the desired mapping     **/
+/**    _pgd_    : address of PGD to use                   **/
+/**    _ptbs_   : address of PGD's PTBs                   **/
+/**    _lstart_ : logical start address of mapping        **/
+/**    _pstart_ : physical start address of mapping       **/
+/**    _size_   : size in bytes of the desired mapping    **/
 /***********************************************************/
-#define krn_forced_map(_pgd_,_ptb_,_lstart_,_pstart_,_size_)						\
-	{																				\
-		for(offset_t offset = 0; offset <= _size_ - 1; offset += PGD_OFFSET) {		\
-			pgd_krn_identity_pde((_pgd_), (_ptb_), _lstart_ + offset);				\
-		}																			\
-		for(offset_t offset = 0; offset <= _size_ - 1; offset += PTB_OFFSET) {		\
-			ptb_krn_forced_pte((_ptb_), _lstart_ + offset, _pstart_ + offset);		\
-		}																			\
+#define krn_forced_map(_pgd_,_ptbs_,_lstart_,_pstart_,_size_)											\
+	{																									\
+		for(offset_t offset = 0; offset <= _size_ - 1; offset += PDE_OFFSET) {							\
+			pgd_set_krn_pde( ((pde32_t*)(_pgd_)), ((pde32_t*)(_ptbs_)), (_lstart_ + offset) );			\
+		}																								\
+		for(offset_t offset = 0; offset <= _size_ - 1; offset += PTE_OFFSET) {							\
+			ptb_set_krn_forced_pte( ((pde32_t*)(_ptbs_)), (_lstart_ + offset), (_pstart_ + offset) );		\
+		}																								\
 	}
 
 
