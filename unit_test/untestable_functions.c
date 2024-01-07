00bc24112190b63360bcc49d44bbe0131e2e6fdf
Zefinder <jakubiak@insa-toulouse.fr>
2024-01-07 22:44:16 +0100

    Fixed tests

--- unit_test/untestable_functions.c
+++ unit_test/untestable_functions.c
@@ -2,6 +2,7 @@
 // We create functions that are kernel based and cannot be tested
 #include <extend/types.h>
 #include <types.h>
+
 void init_task_pagemem(tidx task) {}
 void task_forced_map(tidx task, offset_t virtual_address, offset_t physical_address, int size) {}
 void clear_task_pagemem(tidx task) {}
