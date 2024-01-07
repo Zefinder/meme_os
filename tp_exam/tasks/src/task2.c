d5994bb8a0c89815a593ee849bad4bc50a45dd06
Zefinder <jakubiak@insa-toulouse.fr>
2024-01-07 13:25:44 +0100

    Post merge

--- tp_exam/tasks/src/task2.c
+++ tp_exam/tasks/src/task2.c
@@ -7,6 +7,6 @@ void __attribute__((section(".task2"),aligned(4))) task2(void)
     int counter = 0;
 
     while(1) {
-        Write_shared_memory_syscall((int*) counter_address, counter++);
+        write_shared_memory_syscall((int*) counter_address, counter++);
     }
 }
